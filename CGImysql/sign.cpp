05917f5dc9b3b04a790a99f0475ca2aaf34d7731
zhaoxiaoyu <1961714414@qq.com>
2020-05-07 18:32:13 +0800

    feat: 数据库

--- CGImysql/sign.cpp
+++ CGImysql/sign.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
             cout << "Error:" << mysql_error(con);
             exit(1);
         }
-        con = mysql_real_connect(con, "localhost", "root", "root", "qgydb", 3306, NULL, 0);
+        con = mysql_real_connect(con, "localhost", "root", "zhaoxiaoyu0.0", "MyBench", 3306, NULL, 0);
 
         if (con == NULL)
         {
